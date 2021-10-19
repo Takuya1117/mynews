36b59fb5a2c1d88c6d0c435c53fb6ab10396e469
oosugi takuya <gct1123213@icloud.com>
2021-10-19 06:31:19 +0000

    kadai11

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 
@@ -29,3 +29,7 @@ Route::get('admin/profile/create', 'Admin\ProfileController@add');
 Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
